ca5cfa6d44cef94f02697f13e00dd0e2e3576243
Qurban <qurbanhuseynzade2007@gmail.com>
2026-06-06 08:00:54 +0000

    Add printTeacher function and interface

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -11,6 +11,14 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -30,3 +38,4 @@ const director1: Directors = {
 };
 
 console.log(director1);
+console.log(printTeacher('John', 'Doe'));
